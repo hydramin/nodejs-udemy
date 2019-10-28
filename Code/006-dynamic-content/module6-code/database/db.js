528105168e47e8972a134a613d15ffadd978fac8
Amin Adam <aminmadam1@gmail.com>
2019-10-27 22:07:00 -0400

    note 006 started

--- Code/006-dynamic-content/module6-code/database/db.js
+++ Code/006-dynamic-content/module6-code/database/db.js
@@ -7,12 +7,39 @@ const connection = mysql.createConnection({
     database: 'udemy'
 });
 
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'yourpassword';
 connection.connect((err) => {
     if (err) {
         throw err;
     }
     console.log('Connected');
+
+    // connection.execute('CREATE DATABASE IF NOT EXISTS udemy', (err, result) => {
+    //     if (err) {
+    //         throw err;
+    //     }
+    //     console.log(result, " <-- create db");
+    // });
+
+    // connection.changeUser({ database: 'udemy' }, (err, result) => {
+    //     if (err) throw err;
+    //     console.log(result, " <-- set db");
+    // })
 });
 
-module.exports = connection;
+let create_table = `CREATE TABLE IF NOT EXISTS PRODUCTS (
+                        id INT(5) NOT NULL AUTO_INCREMENT,
+                        product_name VARCHAR(15) NOT NULL DEFAULT "XXX",
+                        product_type VARCHAR(15) DEFAULT NULL, 
+                        amount INT(5) DEFAULT 0,
+                        PRIMARY KEY(id)
+                    )`;
+
+
+connection.execute(create_table, (err, result) => {
+    if (err) throw err;
+    console.log(result, " <-- create table");
+})
+
+module.exports = connection.promise();
 
